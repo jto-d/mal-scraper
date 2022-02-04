66431c88611a72e436a8ecc09ec1c306bcaa0a76
Jack <90474780+jto-d@users.noreply.github.com>
2022-02-04 08:16:55 -0500

    Update Anime.py

--- Anime.py
+++ Anime.py
@@ -3,24 +3,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
 
-# class Anime:
-#     def __init__(self, name, URL) -> None:
-#         self.name = name
-#         self.URL = URL
-#         self.page = urlopen(self.URL).read()
-
-#     def anime_opening(self):
-#         self.page = str(self.page)
-#         self.title = self.page.split('"theme-song-index"')
-#         # self.title = self.page.split('"theme-song-title"')
-#         # self.title = self.title[1].split('"')[1]
-
-#         self.artist = self.page.split('"theme-song-artist"')
-#         self.artist = self.artist[1].split('<')[0][5:]
-
-#     def __str__(self):
-#         return self.name
-
 class Anime:
     def __init__(self, name, URL) -> None:
         self.name = name
@@ -63,9 +45,5 @@ class Anime:
                 else:
                     self.get_opening_info(opening)
                     
-
-    
-
-
     def __str__(self):
         return self.name
