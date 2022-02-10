3402b4a218a736e8b62fc665265d710c6d2eb31b
Jack <90474780+jto-d@users.noreply.github.com>
2022-02-09 22:29:34 -0500

    Add files via upload

--- List.py
+++ List.py
@@ -1,30 +1,60 @@
 from Anime import Anime
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+import requests
 
 
 class List:
     def __init__(self, typ, username) -> None:
-        self.type = typ
+        self.type = typ.upper()
         self.user = username
 
-        self.animes = []
+        self.titles = []
         self.urls = []
 
     def get_anime_url(self, init_url):
-        url = init_url.replace('\\','')
-        url = url.split('"')[1]
-        return 'https://myanimelist.net' + url
+        if self.type == 'MAL':
+            url = init_url.replace('\\','')
+            url = url.split('"')[1]
+            return 'https://myanimelist.net' + url
+        # elif self.type == 'Anilist':
+
 
     def get_anime_title(self, title):
-        anime_name = title.split('"')[1]
-        if anime_name == 'anime_title_eng':
-            return title.split(',',)[0][1:]
+        if self.type == 'MAL':
+            anime_name = title.split('"')[1]
+            if anime_name == 'anime_title_eng':
+                return title.split(',',)[0][1:]
+            
+            # apostrophe instead of text
+            if '&#039;' in anime_name:
+                return anime_name.replace('&#039;', "'")
+            return anime_name
+        # elif self.type == 'ANILIST':
+
+
+
+    def get_anilist_list(self):
         
-        # apostrophe instead of text
-        if '&#039;' in anime_name:
-            return anime_name.replace('&#039;', "'")
-        return anime_name
+        self.url = 'https://anilist.co/user/' + self.user + '/animelist'
+        # req = Request(self.url, headers={'User-Agent': 'Mozilla/5.0'})
+
+        # text = urlopen(req).read()
+        # self.text = text.decode("utf-8")
+        # self.text = self.text
+
+        page = requests.get(self.url)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+        container = soup.find_all('div', class_='list-wrap')[:4]
+
+        self.animes = []
+        
+        self.animes.extend(container[1].select('div.row'))
+        
+            
+                
+
 
     def get_MAL_list(self):
         for i in range(1,5):
@@ -37,7 +67,7 @@ class List:
             titles = text.split('"anime_title"')
             titles.pop(0)
 
-            self.animes.extend([self.get_anime_title(title) for title in titles])
+            self.titles.extend([self.get_anime_title(title) for title in titles])
 
             init_urls = text.split('"anime_url"')
             init_urls.pop(0)
@@ -47,6 +77,8 @@ class List:
     def get_anime_list(self):
         if self.type == 'MAL':
             self.get_MAL_list()
-        # elif self.type == 'Anilist':
+        elif self.type == 'ANILIST':
+            self.get_anilist_list()
+
             
 
--- main.py
+++ main.py
@@ -3,41 +3,10 @@ from Anime import Anime
 from List import List
 import time
 
-lists = List('MAL', 'Lxmonade')
+lists = List('Anilist', 'Lxmonade')
 lists.get_anime_list()
-print(lists.animes)
-
-# ANIME_LIST = 'https://myanimelist.net/animelist/Lxmonade?status=2'
-
-# text = urlopen(ANIME_LIST).read()
-# text = text.decode("utf-8")
-# text = text.replace("&quot;",'"')
-
-# titles = text.split('"anime_title"')
-# titles.pop(0)
-
-# def animeTitle(title):
-#     anime_name = title.split('"')[1]
-#     if anime_name == 'anime_title_eng':
-#         return title.split(',',)[0][1:]
-    
-#     # apostrophe instead of text
-#     if '&#039;' in anime_name:
-#         return anime_name.replace('&#039;', "'")
-#     return anime_name
-
-# animes = [animeTitle(title) for title in titles]
-
-
-# r_URLs = text.split('"anime_url"')
-# r_URLs.pop(0)
-
-# def animeURL(init_URL):
-#     URL = init_URL.replace('\\','')
-#     URL = URL.split('"')[1]
-#     return "https://myanimelist.net" + URL
-
-# URLs = [animeURL(URL) for URL in r_URLs]
+print(len(lists.animes))
+print(lists.animes[0])
 
 # open txt file
 f = open('anime_openings_1.txt', 'w', encoding='utf-8')
@@ -46,26 +15,26 @@ f = open('anime_openings_1.txt', 'w', encoding='utf-8')
 
 # IMPORTANT
 # LIKELY NEED TO ADD A TIMER BECAUSE OF RATE LIMITING
-for i in range(len(animes)):
-    anime = Anime(animes[i], URLs[i])
-    try:
-        anime.anime_opening()
-    except:
-        print('\nMAL LIMIT\n60 SECOND PAUSE\nINDEX NUMBER: ' + i + '\nSUBMIT MAL CHECK\n')
-        time.sleep(60)
-        anime.anime_opening()
-    f.write(str(anime)+'\n')
-    print(str(anime))        
-    if anime.titles != []:
-        print("COMPLETING....")
-        for op in range(len(anime.titles)):
-            f.write(str(op+1) + ': ' + anime.titles[op] + ' by ' + anime.artists[op] + '\n')
-    else:
-        f.write("No Openings\n")
-        print("NO OPENINGS")
-    f.write('\n')
+# for i in range(len(animes)):
+#     anime = Anime(animes[i], urls[i])
+#     try:
+#         anime.anime_opening()
+#     except:
+#         print('\nMAL LIMIT\n60 SECOND PAUSE\nINDEX NUMBER: ' + i + '\nSUBMIT MAL CHECK\n')
+#         time.sleep(60)
+#         anime.anime_opening()
+#     f.write(str(anime)+'\n')
+#     print(str(anime))        
+#     if anime.titles != []:
+#         print("COMPLETING....")
+#         for op in range(len(anime.titles)):
+#             f.write(str(op+1) + ': ' + anime.titles[op] + ' by ' + anime.artists[op] + '\n')
+#     else:
+#         f.write("No Openings\n")
+#         print("NO OPENINGS")
+#     f.write('\n')
 
-f.close()
+# f.close()
 
 
 
